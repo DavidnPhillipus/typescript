70cb68a905b22d5943ec3636bb590c14a11dd6cd
David Phillipus <david.n.phillipus@gmail.com>
2026-08-24 01:47:41 +0200

    feat: add main lesson

---
+++ FirstTypescriptBundler/src/main.ts
@@ -0,0 +1,30 @@
+let a:number = 4
+console.log(a)
+
+let b:string = "I am the best react developer"
+console.log(b)
+
+let c:boolean = true
+console.log(c)
+
+let d:number[] = [1,2,3,4,5]
+console.log(d)
+
+let e:Array<number> = [1,2,3,4,5]
+console.log(e)
+
+let f:[string,number] = ["hello", 1]
+console.log(f)
+
+let g:any = "I can be anything"
+console.log(g)
+
+const h = 10 // this means that the type of h is number and it can only be a 10
+console.log(h)
+
+let i:undefined = undefined
+console.log(i)
+
+let j:null = null
+console.log(j)
+
