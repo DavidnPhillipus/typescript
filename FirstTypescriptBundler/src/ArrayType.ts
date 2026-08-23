7eb254a365534089db748b2d6ea97bd1bde627f4
David Phillipus <david.n.phillipus@gmail.com>
2026-08-24 01:47:42 +0200

    feat: add ArrayType lesson

---
+++ FirstTypescriptBundler/src/ArrayType.ts
@@ -0,0 +1,12 @@
+const a: number[] = [1, 2, 3, 4, 5]
+const b: Array<number> = [1, 2, 3, 4, 5]
+const c:[1,2,3] = [1,2,3]//here we are defining the type of the array that will only be the spesified values
+const d:[number,number,number] = [1,2,3]//here we are defining the type of the array that will only be the spesified values
+const e:string[] = ['a','b','c','d']
+a.push(6)
+b.push(6)
+console.log(c)
+console.log(d)
+console.log(e)
+
+
